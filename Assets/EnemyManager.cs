d55d394dbe3dd865c2df4bd8776f83643bc9c3fe
BuildTools <unconfigured@null.spigotmc.org>
2018-04-10 15:28:23 +0200

    Initial

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public float speed = 10f;
+	
+	private Transform target;
+	private int wavepointIndex = 0;
+	
+	void Start () {
+		transform.position = new Vector3(-5f, 0, 0);
+		target = WaypointManager.points[1];
+	}
+	
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * Time.deltaTime, Space.World);
+		
+		if(Vector3.Distance(transform.position, target.position) <= 0.2f) {
+			GetNextWaypoint();
+		}
+	}
+	
+	void GetNextWaypoint() {
+		wavepointIndex++;
+		target = WaypointManager.points[wavepointIndex];
+	}
+
+}
---
+++ Assets/MoleManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0.36f, -1.31f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y >= 4) {
+			transform.position = new Vector3(0.36f, -1.31f, 0);
+		} else {
+			transform.Translate(Vector3.up * Time.deltaTime, 0);
+		}
+	}
+}
